7d7a655a957d5e325f98e9f88e3e823b4271de8d
Jimmy Kim <jimmykim925@fullsail.edu>
2012-07-31 21:23:58 -0700

    added initial files

---
+++ aoc1/aoc1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  aoc1AppDelegate.h
+//  aoc1
+//
+//  Created by Jimmy Kim on 7/31/12.
+//  Copyright (c) 2012 Jimmy Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aoc1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ aoc1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  aoc1
+//
+//  Created by Jimmy Kim on 7/31/12.
+//  Copyright (c) 2012 Jimmy Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "aoc1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([aoc1AppDelegate class]));
+  }
+}
