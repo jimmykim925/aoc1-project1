576c7550688b294ed26b5e273c213c660033fbbb
Jimmy Kim <jimmykim925@fullsail.edu>
2012-08-02 08:40:29 -0700

    upload updated files

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Jimmy Kim on 8/2/12.
+//  Copyright (c) 2012 Jimmy Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
